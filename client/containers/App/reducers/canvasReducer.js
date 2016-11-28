47e927f5a67ebd532d4a43013fc9e86db8bac54b
Andrew Kaiser <kaisea@rpi.edu>
2016-11-28 18:16:34 -0500

    remove extra reducer

--- client/containers/App/reducers/canvasReducer.js
+++
@@ -1,2 +0,0 @@
-import { fromJS } from 'immutable'
-
--- client/containers/App/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux-immutable'
-
-import canvasDataReducer from './canvasReducer'
-import inputReducer from './inputReducer'
-
-export default combineReducers({
-  canvasDataReducer,
-  inputReducer
-})
