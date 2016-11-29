9ca2b932117c13d80bb13edf4384079cf896643b
Andrew Kaiser <kaisea@rpi.edu>
2016-11-29 18:07:39 -0500

    upload only black or white

--- client/components/Upload/index.jsx
+++ client/components/Upload/index.jsx
@@ -5,12 +5,26 @@
 import React from 'react'
 import {browserHistory} from 'react-router'
 
+let convertToBlackOrWhite = (imgArray) => {
+  var i = 0
+  while (i < imgArray.length) {
+    // ( R + G + B ) / 2
+    var avg = 0.299*imgArray[i] + 0.587*imgArray[i+1] + 0.114*imgArray[i+2]
+    //var avg = ( imgArray[i] + imgArray[i+1] + imgArray[i+2] ) / 3
+    var bOw = avg >= 128 ? 255 : 0
+    imgArray[i] = bOw
+    imgArray[i+1] = bOw
+    imgArray[i+2] = bOw
+    i = i + 4
+  }
+}
+
 const Upload = (props) => {
 
   /**
     Handle submit of an image on to the canvas
     This will update the canvas view
-  */
+    */
   let handleImageChange = (e) => {
     e.preventDefault();
     let state = {}
@@ -23,15 +37,17 @@ const Upload = (props) => {
       //When the image is loaded it will be placed on the canvas
       var image = new Image();
       image.onload = function() {
-          //need to resize canvas to minimum dimensions
-          var canvas = document.createElement('canvas');
-          let ctx = canvas.getContext("2d");
-          canvas.width = image.width;
-          canvas.height = image.height;
-          ctx.drawImage(image, 0, 0);
-          let canvasData = ctx.getImageData(0,0,image.width, image.height);
-          props.setImageData(canvasData);
-          browserHistory.push('/generate');
+        //need to resize canvas to minimum dimensions
+        var canvas = document.createElement('canvas');
+        let ctx = canvas.getContext("2d");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0);
+        let canvasData = ctx.getImageData(0,0,image.width, image.height);
+
+        convertToBlackOrWhite(canvasData.data)
+        props.setImageData(canvasData);
+        browserHistory.push('/generate');
       };
       image.src = state.imagePreviewUrl;
     }
@@ -40,11 +56,11 @@ const Upload = (props) => {
   }
 
   return (
-      <div className="previewComponent">
-            <form onSubmit={(e)=>this._handleSubmit(e)}>
-              <input className="fileInput" type="file" onChange={(e)=>handleImageChange(e)} />
-            </form>
-      </div>
+    <div className="previewComponent">
+      <form onSubmit={(e)=>this._handleSubmit(e)}>
+        <input className="fileInput" type="file" onChange={(e)=>handleImageChange(e)} />
+      </form>
+    </div>
   )
 
 }
